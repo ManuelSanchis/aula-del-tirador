494d6bc2a5992f8258f18c3a035f4caab441bcae
Manuel Sanchis <manuelsanchistormo@gmail.com>
2026-08-17 19:48:58 +0200

    feat: add ActionCard props in TopicsPage

--- app/src/pages/TopicsPage.tsx
+++ app/src/pages/TopicsPage.tsx
@@ -1,7 +1,27 @@
+import { ActionCard } from '@/components/ActionCard';
+import { TOPIC_CARDS } from '@/config/topicCards.config';
+
 export const TopicsPage = () => {
   return (
     <div>
-      <h1>TopicsPage</h1>
+      <header>
+        <h1>Practicar por temas</h1>
+
+        <p>Elige un tema y practica sus preguntas sin límite de tiempo.</p>
+      </header>
+
+      <div>
+        {TOPIC_CARDS.map(({ icon, title, details, to, buttonText }) => (
+          <ActionCard
+            key={to}
+            icon={icon}
+            title={title}
+            details={details}
+            to={to}
+            buttonText={buttonText}
+          />
+        ))}
+      </div>
     </div>
   );
 };
