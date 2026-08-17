fd0660787e19cc1c389afc84d1f3df0e3ee77997
Manuel Sanchis <manuelsanchistormo@gmail.com>
2026-08-17 19:48:37 +0200

    feat: add ActionCard props in HomePage

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,10 +1,29 @@
 import { ActionCard } from '@/components/ActionCard';
+import { HOME_CARDS } from '@/config/homeCards.config';
 
 export const HomePage = () => {
   return (
     <div>
-      <h1>HomePage</h1>
-      <ActionCard />
+      <header>
+        <h1>Bienvenido</h1>
+
+        <p>
+          Practica por temas o simula el examen oficial para obtener la licencia de armas D y E.
+        </p>
+      </header>
+
+      <div>
+        {HOME_CARDS.map(({ icon, title, description, to, buttonText }) => (
+          <ActionCard
+            key={to}
+            icon={icon}
+            title={title}
+            description={description}
+            to={to}
+            buttonText={buttonText}
+          />
+        ))}
+      </div>
     </div>
   );
 };
