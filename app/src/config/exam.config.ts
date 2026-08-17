d8dbd7416246f9bf9173ad051361b9607a17f255
Manuel Sanchis <manuelsanchistormo@gmail.com>
2026-08-17 18:43:19 +0200

    feat: add exam configuration

---
+++ app/src/config/exam.config.ts
@@ -0,0 +1,14 @@
+export const QUESTIONS_PER_TOPIC: Record<number, number> = {
+  1: 4,
+  2: 3,
+  3: 3,
+  4: 4,
+  5: 3,
+  6: 3,
+};
+
+export const EXAM_TOTAL_QUESTIONS = 20;
+
+export const EXAM_TIME_LIMIT_MINUTES = 20;
+
+export const EXAM_PASSING_SCORE = 16;
