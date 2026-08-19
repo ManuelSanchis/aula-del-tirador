a465785997b7411f439653b6e962429854c852df
Manuel Sanchis <manuelsanchistormo@gmail.com>
2026-08-19 17:51:36 +0200

    feat: make Sidebar collapsible

--- app/src/layout/MainLayout.tsx
+++ app/src/layout/MainLayout.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Sidebar } from './Sidebar';
 
 export const MainLayout = () => {
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarCollapsed((previousState) => !previousState);
+  };
+
   return (
     <div>
-      <Sidebar />
+      <Sidebar isCollapsed={isSidebarCollapsed} onToggle={toggleSidebar} />
+
       <main>
         <Outlet />
       </main>
