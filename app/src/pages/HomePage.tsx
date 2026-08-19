8ad64bbda129480bf14c4380d3976c1ed323f36b
Manuel Sanchis <manuelsanchistormo@gmail.com>
2026-08-19 18:51:08 +0200

    feat: add HomePage styles

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,18 +1,20 @@
 import { ActionCard } from '@/components/ActionCard';
 import { HOME_CARDS } from '@/config/homeCards.config';
 
+import styles from './HomePage.module.css';
+
 export const HomePage = () => {
   return (
-    <div>
-      <header>
-        <h1>Bienvenido</h1>
+    <div className={styles.page}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Bienvenido</h1>
 
-        <p>
+        <p className={styles.description}>
           Practica por temas o simula el examen oficial para obtener la licencia de armas D y E.
         </p>
       </header>
 
-      <div>
+      <div className={styles.cards}>
         {HOME_CARDS.map(({ icon, title, description, to, buttonText }) => (
           <ActionCard
             key={to}
